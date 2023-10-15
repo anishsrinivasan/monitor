b8a9bb3780a3901f16074f124e8f25a6ef12a061
Anish <itsanishsrinivasan@gmail.com>
2023-10-15 18:52:58 +0530

    fix : remove discord client

--- src/lib/discord/index.ts
+++ src/lib/discord/index.ts
@@ -1,8 +1,3 @@
-import { Client, GatewayIntentBits } from "discord.js";
 import { sendMessage, sendErrorMessage } from "./message";
 
-const token = process.env.DISCORD_TOKEN || "";
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.login(token);
-
-export { client, sendMessage, sendErrorMessage };
+export { sendMessage, sendErrorMessage };
